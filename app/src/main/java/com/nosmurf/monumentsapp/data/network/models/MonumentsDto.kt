ceb0977a36f992cfe94b804d607850b8220f335a
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2017-11-07 00:01:15 +0100

    Solving naming mistake on MonumentsDto

--- app/src/main/java/com/nosmurf/monumentsapp/data/network/models/MonumentsDto.kt
+++ app/src/main/java/com/nosmurf/monumentsapp/data/network/models/MonumentsDto.kt
@@ -2,52 +2,52 @@ package com.nosmurf.monumentsapp.data.network.models
 
 
 data class MonumentsDto(
-        val head: Head,
-        val results: Results
+        val headDto: HeadDto,
+        val resultsDto: ResultsDto
 )
 
-data class Results(
+data class ResultsDto(
         val distinct: Boolean, //false
         val ordered: Boolean, //true
-        val bindings: List<Binding>
+        val bindingDtos: List<BindingDto>
 )
 
-data class Binding(
-        val lng: Lng,
-        val lat: Lat,
-        val om_tipoMonumento: OmTipoMonumento,
-        val name: Name,
-        val om_tieneEnlaceSIG: OmTieneEnlaceSIG
+data class BindingDto(
+        val lngDto: LngDto,
+        val latDto: LatDto,
+        val om_tipoMonumentoDto: OmTipoMonumentoDto,
+        val nameDto: NameDto,
+        val om_tieneEnlaceSIGDto: OmTieneEnlaceSIGDto
 )
 
-data class OmTieneEnlaceSIG(
+data class OmTieneEnlaceSIGDto(
         val type: String, //uri
         val value: String //http://sig.caceres.es/serweb/fichasig/fichatoponimia.php?mslink=1163
 )
 
-data class OmTipoMonumento(
+data class OmTipoMonumentoDto(
         val type: String, //literal
         val value: String //Concatedral
 )
 
-data class Name(
+data class NameDto(
         val type: String, //literal
         val value: String //Concatedral de Santa Maria
 )
 
-data class Lng(
+data class LngDto(
         val type: String, //typed-literal
         val datatype: String, //http://www.w3.org/2001/XMLSchema#decimal
         val value: String //-6.37004029561
 )
 
-data class Lat(
+data class LatDto(
         val type: String, //typed-literal
         val datatype: String, //http://www.w3.org/2001/XMLSchema#decimal
         val value: String //39.4745659792
 )
 
-data class Head(
+data class HeadDto(
         val link: List<Any>,
         val vars: List<String>
 )
